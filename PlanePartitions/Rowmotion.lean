21ef93e1ef2bdef955ea280ee51e418be85bafbb
Jimmy Ostler <jtostler1@gmail.com>
2025-02-22 20:36:24 -0800

    temp

--- PlanePartitions/Rowmotion.lean
+++ PlanePartitions/Rowmotion.lean
@@ -2,5 +2,24 @@ import PlanePartitions.Partition
 
 open PlanePartition
 def rowmotion (p : PlanePartition n) : PlanePartition n :=
+  let minimalElements := 
+      (List.range n).filter (λ i =>
+        (List.range n).filter (λ j =>
+          p.data[i][j] = 0 ∧ 
+          (i = 0 ∨ p.data[i - 1][j] > 0) ∧ 
+          (j = 0 ∨ p.data[i][j - 1] > 0)
+        )
+      )
+
+  let newData := 
+    p.data.map (λ row =>
+      row.map (λ x =>
+        if minimalElements.contains x then x + 1 else x
+      )
+    )
+
+  ⟨newData, 
+    λ i j => by simp; exact p.invariant_col i j, 
+    λ i j => by simp; exact p.invariant_row i j⟩
 
 
