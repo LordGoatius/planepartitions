724db5b5ea62d1eef3b8bd83fcdae7ffa3b0ea02
Jimmy Ostler <jtostler1@gmail.com>
2025-02-22 22:11:43 -0800

    rowmotion

--- PlanePartitions/Rowmotion.lean
+++ PlanePartitions/Rowmotion.lean
@@ -2,24 +2,17 @@ import PlanePartitions.Partition
 
 open PlanePartition
 def rowmotion (p : PlanePartition n) : PlanePartition n :=
-  let minimalElements := 
-      (List.range n).filter (λ i =>
-        (List.range n).filter (λ j =>
-          p.data[i][j] = 0 ∧ 
-          (i = 0 ∨ p.data[i - 1][j] > 0) ∧ 
-          (j = 0 ∨ p.data[i][j - 1] > 0)
-        )
-      )
+  -- arr.size > 1 &&
+  -- (List.finRange (n - 1)).all (fun i =>
+  --   (List.finRange n).all (fun j =>
+  --     arr[i][j] ≥ arr[i + (1 : Nat)][j]
+  --   )
+  -- ) &&
+  -- (List.finRange n).all (fun i =>
+  --   (List.finRange (n - 1)).all (fun j =>
+  --     arr[i][j] ≥ arr[i][j + (1 : Nat)]
+  --   )
+  -- )
 
-  let newData := 
-    p.data.map (λ row =>
-      row.map (λ x =>
-        if minimalElements.contains x then x + 1 else x
-      )
-    )
-
-  ⟨newData, 
-    λ i j => by simp; exact p.invariant_col i j, 
-    λ i j => by simp; exact p.invariant_row i j⟩
-
-
+  -- NOTE: Minimal elements not in
+  -- Generate partition from there
